6aaf2ade29cd3a15f851202d4985cbcf215d7475
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-01 20:58:01 -0400

    feat: add simple cli tool
    This avoids using Yargs or any other such dependency since the main
    point of this package is to be used in JS projects, and it does not make
    sense to bloat it with CLI dependencies.

---
+++ src/cli.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import process from 'process';
+import {parse} from './parse';
+
+const HELP = `Parse and format NEAR tokens and gas units. Examples:
+
+    near-units 10 N # => 10000000000000000000000000
+    near-units -h 10000000000000000000000000N # => 10 N
+    near-units 50 Tgas # => 50000000000000
+    near-units -h 50000000000000 gas # => 50 Tgas
+`;
+
+let args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log(HELP);
+  process.exit(0);
+}
+
+let wantsHuman = false;
+if (args.length > 1 && args.includes('-h')) {
+  wantsHuman = true;
+  args = args.filter((x) => x !== '-h');
+}
+
+const input = args.join('');
+
+if (input === '--help') {
+  console.log(HELP);
+  process.exit(0);
+}
+
+try {
+  if (wantsHuman) {
+    console.log(parse(input).toHuman());
+  } else {
+    console.log(parse(input).toJSON());
+  }
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  } else {
+    console.error(error);
+  }
+
+  process.exit(1);
+}
